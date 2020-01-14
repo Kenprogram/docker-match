aadbbbc0b9a4cde9a58adfd60205a48f4cd0f88f
多田健太 <tadakenta@tadakentanoMacBook-Pro.local>
2020-01-14 18:32:53 +0900

    controller matching作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :users, only: %i(index show)
   resources :reactions, only: %i(create)
-
+  resources :matching, only: %i(index)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
